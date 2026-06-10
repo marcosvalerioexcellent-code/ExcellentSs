b707658447808580b11c899106f683fc27cd9a88
marcosvalerioexcellent-code <marcosvalerioexcellent@gmail.com>
2026-06-09 23:41:13 -0300

    Update meta.js

--- api/meta.js
+++ api/meta.js
@@ -1,17 +1,19 @@
 // Arquivo: /api/meta.js
 
 export default async function handler(req, res) {
-  // CREDENCIAIS DA API
+  // CREDENCIAIS DA API (Mantenha as suas chaves aqui)
   const ACCESS_TOKEN = "EAAO1kaxzrSQBRsd535Xc0jFVSurw9c3vMGeY7lX4Ws6eI8MiZCorCn2TrDkVGxfEZBeaPO4WKeGP7Th32sFf8KwjFPpBh4x4nwIwFQmWohnHXfXMZAEbI1eNLmFXfEZBgXDh64ivx3uLSfp2onBvKgkyteZCHoe1iGG4mMPBI56CoL1xknNR20yiZA42JAvBEWPlPIZAXzaWxPp5feLZA5t71qkODrPlNjI30IZAYYQO2D2XZCXHwvTj9Vmk3R7NQalNid4kIGiyM6T5VE8yvWZBAqE";
   const APP_ID = "1044062198279460";
   const APP_SECRET = "33d10a23b4407496726bc8e0d0850403";
   
-  // ATENÇÃO: Substitua o valor abaixo pelo ID da sua Conta de Anúncios!
+  // O seu ID que já está funcionando
   const AD_ACCOUNT_ID = "754104033807907"; 
 
+  // Pega o filtro selecionado no painel (se não tiver, usa 30 dias por padrão)
+  const preset = req.query.preset || 'last_30d';
+
   try {
-    // Busca os dados de desempenho diário dos últimos 30 dias
-    const url = `https://graph.facebook.com/v19.0/act_${AD_ACCOUNT_ID}/insights?time_increment=1&date_preset=last_30d&fields=spend,clicks,impressions,cpc,ctr&access_token=${ACCESS_TOKEN}`;
+    const url = `https://graph.facebook.com/v19.0/act_${AD_ACCOUNT_ID}/insights?time_increment=1&date_preset=${preset}&fields=spend,clicks,impressions,cpc,ctr&access_token=${ACCESS_TOKEN}`;
     
     const response = await fetch(url);
     const data = await response.json();
